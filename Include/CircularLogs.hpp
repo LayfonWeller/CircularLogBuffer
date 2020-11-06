56ebb5e034a37c50ff8ef1848b431b9ae91e5218
Mathieu Giguere <mathieu.giguere@umanomedical.com>
2020-11-05 20:59:53 -0500

    First commit

---
+++ Include/CircularLogs.hpp
@@ -0,0 +1,227 @@
+#pragma once
+
+#include <array>
+#include <limits>
+#include <utility>
+#include <stdexcept>
+
+#include "CircularLogsIterator.hpp"
+
+template <typename Type, size_t T_max_size> struct CircularLogs {
+
+    using size_type              = uint8_t;
+    using value_type             = Type;
+    using difference_type        = std::ptrdiff_t;
+    using reference              = value_type &;
+    using const_reference        = const value_type &;
+    using pointer                = value_type *;
+    using const_pointer          = const value_type *;
+    using iterator               = LogIterator<value_type, T_max_size>;
+    using const_iterator         = LogIterator<const value_type, T_max_size>;
+    using reverse_iterator       = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+    [[nodiscard]] constexpr iterator begin()
+    {
+        return iterator(m_container.data(), (m_full ? m_index : 0), false);
+    }
+    [[nodiscard]] constexpr iterator end()
+    {
+        return iterator(m_container.data(), m_index, m_full);
+    }
+    [[nodiscard]] constexpr const_iterator cbegin() const
+    {
+        return const_iterator(m_container.data(), (m_full ? m_index : 0),
+                              false);
+    }
+    [[nodiscard]] constexpr const_iterator cend() const
+    {
+        return const_iterator(m_container.data(), m_index, m_full);
+    }
+    [[nodiscard]] constexpr reverse_iterator rbegin()
+    {
+        return std::make_reverse_iterator(end());
+    }
+    [[nodiscard]] constexpr reverse_iterator rend()
+    {
+        return std::make_reverse_iterator(begin());
+    }
+    [[nodiscard]] constexpr const_reverse_iterator crbegin() const
+    {
+        return std::make_reverse_iterator(cend());
+    }
+    [[nodiscard]] constexpr const_reverse_iterator crend() const
+    {
+        return std::make_reverse_iterator(cbegin());
+    }
+
+    constexpr reference operator[](const difference_type &diff) noexcept
+    {
+        return *std::next(begin(), diff);
+    }
+    constexpr const_reference operator[](const difference_type &diff) const noexcept
+    {
+        return *std::next(cbegin(), diff);
+    }
+
+    [[nodiscard]] constexpr bool is_valid_index(const difference_type &diff) const {
+        return diff < size() && diff >= 0;
+    }
+
+    [[nodiscard]] constexpr reference at(const difference_type &diff)
+    {
+        if (is_valid_index(diff))
+            return *std::next(begin(), diff);
+        throw std::out_of_range("Invalid range required");
+    }
+
+    [[nodiscard]] constexpr const_reference at(const difference_type &diff) const
+    {
+        if (is_valid_index(diff))
+            return *std::next(cbegin(), diff);
+        throw std::out_of_range("Invalid range required");
+    }
+
+    [[nodiscard]] constexpr bool get(const difference_type &diff, reference value)
+    {
+        if (is_valid_index(diff)) {
+            value = *std::next(begin(), diff);
+            return true;
+        }
+        return false;
+    }
+
+    [[nodiscard]] constexpr bool get(const difference_type &diff, const_reference value) const
+    {
+        if (is_valid_index(diff)) {
+            value = *std::next(cbegin(), diff);
+            return true;
+        }
+        return false;
+    }
+
+    [[nodiscard]] constexpr reference r_at(const difference_type &diff)
+    {
+        if (is_valid_index(diff))
+            return *std::next(rbegin(), diff);
+        throw std::out_of_range("Invalid range required");
+    }
+
+    [[nodiscard]] constexpr const_reference r_at(const difference_type &diff) const
+    {
+        if (is_valid_index(diff))
+            return *std::next(crbegin(), diff);
+        throw std::out_of_range("Invalid range required");
+    }
+
+    [[nodiscard]] constexpr bool r_get(const difference_type &diff, reference value)
+    {
+        if (is_valid_index(diff)) {
+            value = *std::next(rbegin(), diff);
+            return true;
+        }
+        return false;
+    }
+
+    [[nodiscard]] constexpr bool r_get(const difference_type &diff, const_reference value) const
+    {
+        if (is_valid_index(diff)) {
+            value = *std::next(crbegin(), diff);
+            return true;
+        }
+        return false;
+    }
+
+    constexpr void push_back(const value_type &a)
+    {
+        m_container.at(m_index++) = a;
+        if (m_index == T_max_size) {
+            m_index = 0;
+            m_full  = true;
+        }
+    }
+    constexpr void push_back(value_type &&a)
+    {
+        m_container.at(m_index++) = std::move(a);
+
+        if (m_index == T_max_size) {
+            m_index = 0;
+            m_full  = true;
+        }
+    }
+
+    [[nodiscard]] constexpr size_type max_size() const noexcept
+    {
+        return T_max_size;
+    }
+
+    [[nodiscard]] constexpr size_type size() const noexcept
+    {
+        if (m_full)
+            return T_max_size;
+        return m_index;
+    }
+
+    constexpr pointer data() noexcept { return m_container.data(); }
+
+    constexpr const_pointer data() const noexcept { return m_container.data(); }
+
+    constexpr bool empty() const noexcept
+    {
+        if (m_full)
+            return false;
+        return m_index == 0;
+    }
+
+    constexpr void clear() noexcept
+    {
+        m_full  = false;
+        m_index = 0;
+    }
+
+  public:
+    bool                               m_full  = false;
+    uint8_t                            m_index = 0;
+    std::array<value_type, T_max_size> m_container;
+};
+
+template <typename Type, size_t T_max_size,
+          typename WrappingIndexType = uint8_t>
+struct IndexedCircularLogs
+    : public CircularLogs<std::pair<WrappingIndexType, Type>, T_max_size> {
+
+    using key_type    = WrappingIndexType;
+    using mapped_type = Type;
+    using value_type  = std::pair<key_type, mapped_type>;
+
+    key_type m_wrappingIndex;
+
+    constexpr IndexedCircularLogs(
+        const key_type t_wrappingIndex =
+            std::numeric_limits<key_type>::max()) noexcept
+        : m_wrappingIndex(std::move(t_wrappingIndex))
+    {
+    }
+    ~IndexedCircularLogs() noexcept = default;
+    constexpr IndexedCircularLogs(const IndexedCircularLogs &other) noexcept =
+        default;
+    constexpr IndexedCircularLogs(IndexedCircularLogs &&other) noexcept =
+        default;
+    // cppcheck-suppress operatorEq
+    constexpr IndexedCircularLogs &
+    operator=(const IndexedCircularLogs &other) noexcept = default;
+    // cppcheck-suppress operatorEq
+    constexpr IndexedCircularLogs &
+    operator=(IndexedCircularLogs &&other) noexcept = default;
+
+    constexpr void push_back(const mapped_type &a)
+    {
+        CircularLogs<value_type, T_max_size>::push_back(
+            std::make_pair(++m_wrappingIndex, a));
+    }
+    constexpr void push_back(mapped_type &&a)
+    {
+        CircularLogs<value_type, T_max_size>::push_back(
+            std::make_pair(++m_wrappingIndex, std::move(a)));
+    }
+};
---
+++ Include/CircularLogsIterator.hpp
@@ -0,0 +1,256 @@
+#if defined(DEBUG) && defined(DEBUG_LOGITERATOR) && DEBUG_LOGITERATOR
+// #include <assert.h>
+
+#pragma GCC push_options
+#pragma GCC optimize("O0")
+
+/* #define DEBUG_PART(a)                                                          \
+     {                                                                          \
+         printf("[%s:%d] num=%lu Second Half = %s\n", __FILE__, __LINE__,       \
+                a->num, (a->partOfSecondHalf ? "true" : "false"));              \
+     }*/
+#define DEBUG_PART(a)                                                          \
+    {                                                                          \
+        assert(num >= 0 && num < max_size);                                    \
+    }
+#else
+#ifdef DEBUG_LOGITERATOR
+#undef DEBUG_LOGITERATOR
+#endif
+#define DEBUG_LOGITERATOR 0
+#define DEBUG_PART(a)                                                          \
+    {                                                                          \
+    }
+#endif // DEBUG
+
+#include <iterator>
+
+template <typename Type, size_t max_size> class LogIterator
+{
+  public:
+    // iterator traits
+    using difference_type   = ptrdiff_t;
+    using value_type        = Type;
+    using pointer           = value_type *;
+    using reference         = value_type &;
+    using const_pointer     = const value_type *;
+    using const_reference   = const value_type &;
+    using iterator_category = std::random_access_iterator_tag;
+
+  private:
+    pointer root;
+    size_t  num = 0;
+    bool    partOfSecondHalf =
+        false; // part of the second half is it's after the roll over point
+               // (between 0 and index if logged filled)
+
+    constexpr void decrimentIndex(const difference_type &t_howMuch) noexcept
+    {
+        DEBUG_PART(this);
+        if (t_howMuch < 0)
+            return incrementIndex(-t_howMuch);
+        if (t_howMuch == 0)
+            return;
+        const int newNum = num - t_howMuch;
+#if DEBUG_LOGITERATOR
+        const auto old = num;
+#endif
+        if (newNum < 0) {
+#if DEBUG_LOGITERATOR
+            assert(partOfSecondHalf == true);
+#endif
+            partOfSecondHalf = false;
+            num              = (max_size + (newNum));
+        } else {
+            num = newNum;
+        }
+#if DEBUG_LOGITERATOR
+        assert(num < max_size);
+        // printf("[%s:%d] Decrease NUM from %lu by %lu, to %lu\n", __FILE__,
+        //        __LINE__, old, t_howMuch, num);
+#endif
+    }
+    constexpr void incrementIndex(const difference_type &t_howMuch) noexcept
+    {
+        DEBUG_PART(this);
+        if (t_howMuch < 0)
+            return decrimentIndex(-t_howMuch);
+        if (t_howMuch == 0)
+            return;
+        const size_t newNum = num + t_howMuch;
+#if DEBUG_LOGITERATOR
+        const auto old = num;
+#endif
+        if (newNum >= max_size) {
+#if DEBUG_LOGITERATOR
+            assert(partOfSecondHalf == false);
+#endif
+            partOfSecondHalf = true;
+            num              = newNum - max_size;
+        } else {
+            num = newNum;
+        }
+#if DEBUG_LOGITERATOR
+        // printf("[%s:%d] Increased NUM from %lu by %lu, to %lu\n", __FILE__,
+        //        __LINE__, old, t_howMuch, num);
+        assert(num < max_size);
+#endif
+    }
+
+  public:
+    constexpr LogIterator(pointer t_root, int t_num, bool t_partOfSecondHalf)
+        : root(t_root), num(t_num), partOfSecondHalf(t_partOfSecondHalf)
+    {
+
+        if (num > max_size) {
+            num += max_size;
+        }
+#if DEBUG_LOGITERATOR
+        DEBUG_PART(this);
+        assert(num < max_size);
+#endif
+    }
+    constexpr LogIterator(const LogIterator<Type, max_size> &rawIterator) =
+        default;
+    constexpr LogIterator &operator++() noexcept
+    {
+        DEBUG_PART(this);
+        incrementIndex(1u);
+        return *this;
+    }
+    constexpr LogIterator operator++(int) noexcept
+    {
+        DEBUG_PART(this);
+        LogIterator retval = *this;
+        ++(*this);
+        return retval;
+    }
+    constexpr LogIterator &operator--() noexcept
+    {
+        DEBUG_PART(this);
+        decrimentIndex(1u);
+        return *this;
+    }
+    constexpr LogIterator operator--(int) noexcept
+    {
+        LogIterator retval = *this;
+        --(*this);
+        return retval;
+    }
+    [[nodiscard]] constexpr LogIterator &
+    operator+=(const difference_type &movement) noexcept
+    {
+        DEBUG_PART(this);
+        incrementIndex(movement);
+        return (*this);
+    }
+    [[nodiscard]] constexpr LogIterator<Type, max_size>
+    operator-=(const difference_type &movement) noexcept
+    {
+        DEBUG_PART(this);
+        decrimentIndex(movement);
+        return (*this);
+    }
+    [[nodiscard]] constexpr bool
+    operator==(const LogIterator &other) const noexcept
+    {
+        return (num == other.num)
+               && (partOfSecondHalf == other.partOfSecondHalf);
+    }
+    [[nodiscard]] constexpr bool
+    operator!=(const LogIterator &other) const noexcept
+    {
+        return !(*this == other);
+    }
+    // cppcheck-suppress functionConst // Can't be cost need additional overload
+    // for const otherwise the value returned is editable
+    [[nodiscard]] constexpr reference       operator*() { return root[num]; }
+    [[nodiscard]] constexpr const_reference operator*() const
+    {
+        return root[num];
+    }
+    // cppcheck-suppress functionConst // Can't be cost need additional overload
+    // for const otherwise the value returned is editable
+    [[nodiscard]] constexpr pointer        operator->() { return &root[num]; }
+    [[nodiscard]] constexpr const_pointer *operator->() const
+    {
+        return &root[num];
+    }
+    [[nodiscard]] constexpr difference_type
+    operator-(const LogIterator &b) const noexcept
+    {
+        DEBUG_PART(this);
+        DEBUG_PART((&b));
+
+        const auto thisModNum = this->partOfSecondHalf * max_size + this->num;
+        const auto bModNum    = b.partOfSecondHalf * max_size + b.num;
+
+        difference_type ret = thisModNum - bModNum;
+
+#if DEBUG_LOGITERATOR
+        // printf("[%s:%d] (this->num=%lu->%lu, b.num=%lu->%lu)\n", __FILE__,
+        //        __LINE__, this->num, thisModNum, b.num, bModNum);
+        // printf("[%s:%d] distance=%ld (this->num=%lu, b.num=%lu)\n", __FILE__,
+        //        __LINE__, ret, this->num, b.num);
+#endif
+        return ret;
+    }
+
+    [[nodiscard]] constexpr LogIterator
+    operator-(const difference_type &movement) const noexcept
+    {
+        difference_type distance = this->num - movement;
+        const bool      nowPartOfFirstHalf =
+            (static_cast<difference_type>(this->num) - movement) < 0;
+        if (nowPartOfFirstHalf) {
+            distance += max_size;
+        }
+#if DEBUG_LOGITERATOR
+        // printf("[%s:%d] this->num=%lu, partOfSecondHalf=%s, movement=%ld,
+        //        distance           = % ld,
+        //        nowPartOfFirstHalf = % s,
+        //        assert             = % s\n ", __FILE__,
+        //                 __LINE__,
+        //        this->num, (this->partOfSecondHalf ? "true" : "false"),
+        //        movement, distance, (nowPartOfFirstHalf ? "true" : "false"),
+        //        !(!this->partOfSecondHalf && nowPartOfFirstHalf) ? "false"
+        //                                                         : "true");
+        assert(!(!this->partOfSecondHalf && nowPartOfFirstHalf));
+#endif
+        return LogIterator(root, distance,
+                           nowPartOfFirstHalf ? false : this->partOfSecondHalf);
+    }
+
+    [[nodiscard]] constexpr LogIterator<Type, max_size>
+    operator+(const size_t b) const noexcept
+    {
+        const difference_type distance            = (this->num + b) % max_size;
+        const bool            nowPartOfSecondHalf = this->num + b >= max_size;
+#if DEBUG_LOGITERATOR
+        assert(!(this->partOfSecondHalf && nowPartOfSecondHalf));
+        // printf("[%s:%d] num=%lu (this->num(%lu) + b(%lu))%%max_size\n",
+        //        __FILE__, __LINE__, distance, this->num, b);
+#endif
+        return LogIterator<Type, max_size>(
+            root, distance, this->partOfSecondHalf || nowPartOfSecondHalf);
+    }
+
+    [[nodiscard]] constexpr pointer       getPtr() const { return &root[num]; }
+    [[nodiscard]] constexpr const_pointer getConstPtr() const
+    {
+        return &root[num];
+    }
+
+
+    bool operator<(const LogIterator<Type, max_size>& t_second) const {
+        if (t_second.partOfSecondHalf == true && this->partOfSecondHalf == false)
+            return true;
+        if (t_second.partOfSecondHalf == false && this->partOfSecondHalf == true)
+            return false;
+        return this->num < t_second.num;
+    }
+};
+
+#if defined(DEBUG) && defined(DEBUG_LOGITERATOR) && DEBUG_LOGITERATOR
+#pragma GCC pop_options
+#endif
